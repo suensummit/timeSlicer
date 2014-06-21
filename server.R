0b2b281821cdd212e914cd5e826a3bcd13de54e3
Summit Suen <summit.suen@gmail.com>
2014-06-22 01:00:11 +0800

    First Edition
    simple YouTube video-slicer

--- server.R
+++ server.R
@@ -1,3 +1,10 @@
-shinyServer(function(input, output, session) {
-  
-})
+# server.R
+
+shinyServer(
+  function(input, output) {
+    output$newlinks <- renderText({ 
+      input$sliceButton
+      isolate(paste0(input$oldlink, "&t=", input$div_time, "m", input$ovl_time, "s"))
+    })
+  }
+)
--- ui.R
+++ ui.R
@@ -1,15 +1,16 @@
 # ui.R
-#source("url-input.R")
-library(shiny)
 
 shinyUI(fluidPage(
-  titlePanel("Simple Youtube Time-Slicer"),
+  titlePanel(img(src="test_logo3.png", height = 175, width = 640),
+             "Simple YouTube Video-Slicer"
+             ),
   sidebarPanel(h1("Original Link"),
-               textInput("olink", "URL: ", "http://www.youtube.com/"),
-               numericInput("div_time", "video length (min): ", 3, min = 0, max = 10),
-               numericInput("ovl_time", "overlay time (sec): ", 30, min = 0, max = 60)
+               textInput("oldlink", "URL: ", "http://www.youtube.com/"),
+               numericInput("div_time", "video length (min): ", 3, min = 0),
+               numericInput("ovl_time", "overlay time (sec): ", 30, min = 0),
+               p(actionButton("sliceButton", "Slice!"))
                ),
-
-  mainPanel(h1("Sliced Links:")
+  mainPanel(h1("Sliced Links:"),
+            textOutput("newlinks")
             )
 ))
